bb65e3f6ecb9ada110ca86c587b1ae49313ff682
luchinsol1590 <luchinsol1590@gmail.com>
2024-07-12 15:41:21 -0500

    resp

--- lib/componentes/prelogin.dart
+++ lib/componentes/prelogin.dart
@@ -11,55 +11,134 @@ class Prelogin extends StatefulWidget {
 class _PreloginState extends State<Prelogin> {
   @override
   Widget build(BuildContext context) {
-    final anchoActual = MediaQuery.of(context).size.width;
-    final largoActual = MediaQuery.of(context).size.height;
-
     return Scaffold(
-      
-      body: Column(
+      body: Stack(
         children: [
-          LayoutBuilder(
-            builder: (context, constraints) {
-              if (constraints.maxWidth <= Breakpoint.xsmall) {
-                return Text(
-                  "xsmall",
-                  style: TextStyle(fontSize: 15),
-                );
-              } else if (constraints.maxWidth <= Breakpoint.small) {
-                return Text(
-                  "small",
-                  style: TextStyle(fontSize: 20),
-                );
-              } else if (constraints.maxWidth <= Breakpoint.medium) {
-                return Text(
-                  "medium",
-                  style: TextStyle(fontSize: 25),
-                );
-              } else if (constraints.maxWidth <= Breakpoint.large) {
-                return _sollogo();
-              } else {
-                return Text(
-                  "out range",
-                  style: TextStyle(fontSize: 60),
-                );
-              }
-            },
+          Container(
+            decoration:const  BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/imagenes/azul.jpg'),
+                fit: BoxFit.cover, // Cambiado a BoxFit.cover para que cubra todo el Container
+              ),
+            ),
           ),
-          
-              
+          Column(
+            children: [
+              LayoutBuilder(
+                builder: (context, constraints) {
+                  if (constraints.maxWidth <= Breakpoint.xsmall) {
+                    return _sollogo();
+                    
+                  } else if (constraints.maxWidth <= Breakpoint.small) {
+                    return _sollogo1();
+                  } else if (constraints.maxWidth <= Breakpoint.medium) {
+                    return _sollogo2();
+                  } else if (constraints.maxWidth <= Breakpoint.large) {
+                    return _sollogo3();
+                  } else {
+                    return _sollogo4();/*Text(
+                      "out range",
+                      style: TextStyle(fontSize: 60),
+                    );*/
+                  }
+                },
+              ),
+            ],
+          ),
         ],
       ),
     );
   }
-  Widget _sollogo(){
+
+  Widget _sollogo() {
     return Center(
       child: Container(
-        width: 350,
-        height: 500,
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('lib/imagenes/aguita.jpg'))
+        width: 200,
+        height: 200,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/imagenes/nuevecito.png'),
+            fit: BoxFit.contain,
+            
+          ),
         ),
       ),
+    );
+  }
+  Widget _sollogo1() {
+    return Center(
+      child: Container(
+        width: 250,
+        height: 250,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/imagenes/nuevecito.png'),
+            fit: BoxFit.contain,
+            
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _sollogo2() {
+    return Center(
+      child: Container(
+        //width: 350,
+        height: 300,
+        decoration:const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/imagenes/nuevecito.png'),
+           fit: BoxFit.contain,
+            
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _sollogo3() {
+    return Center(
+      child: Container(
+        //width: 350,
+        height: 350,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lib/imagenes/nuevecito.png'),
+           fit: BoxFit.contain,
+            
+          ),
+        ),
+      ),
+    );
+  }
+  Widget _sollogo4() {
+    return Column(
+      children: [
+        Center(
+          child: Container(
+            //width: 350,
+            height: 400,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('lib/imagenes/nuevecito.png'),
+                fit: BoxFit.contain,
+                
+              ),
+            ),
+          ),
+        ),
+        const Center(
+          child: Text("Bienvenido a la",
+          style: TextStyle(
+            fontSize: 69,
+            color: Colors.white,fontWeight: FontWeight.w700),),
+        ),
+       const Center(
+          child: Text("Familia Sol",
+          style: TextStyle(
+            fontSize: 69,
+            color: Colors.white,fontWeight: FontWeight.w700),),
+        ),
+      ],
     );
   }
 }
