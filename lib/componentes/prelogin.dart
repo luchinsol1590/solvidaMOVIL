263ff6d1f74d6b0c63f8c8166cb242a1cc96c417
luchinsol1590 <luchinsol1590@gmail.com>
2024-07-14 01:17:16 -0500

    final UIUX

--- lib/componentes/prelogin.dart
+++ lib/componentes/prelogin.dart
@@ -2,44 +2,140 @@ import 'package:flutter/material.dart';
 import 'package:solvida/componentes/responsiveUI/breakpoint.dart';
 
 class Prelogin extends StatefulWidget {
-  const Prelogin({Key? key}) : super(key: key);
+  const Prelogin({super.key});
 
   @override
   State<Prelogin> createState() => _PreloginState();
 }
 
 class _PreloginState extends State<Prelogin> {
-   
-   Widget sollogo(double ancho,double alto,double texto) {
-    return Column(
-      children: [
-        Center(
-          child: Container(
-            width: ancho,
-            height: alto,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('lib/imagenes/nuevecito.png'),
-                fit: BoxFit.contain,
-                
+  Widget sollogo(String tama, double ancho, double actual, double alto,
+      double texto1, double texto2, double botones, double textoboton) {
+    return Container(
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Text(
+              tama,
+              style: const TextStyle(color: Colors.white),
+            ),
+            Text(
+              "$actual",
+              style: const TextStyle(color: Colors.white),
+            ),
+            Expanded(
+              child: Container(
+                  //color: const Color.fromARGB(255, 145, 144, 144),
+                  child: Column(
+                children: [
+                  Center(
+                    child: Container(
+                      width: ancho,
+                      height: alto,
+                      decoration: const BoxDecoration(
+                        //color: Colors.grey,
+                        image: DecorationImage(
+                          image: AssetImage('lib/imagenes/nuevecito.png'),
+                          fit: BoxFit.contain,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Center(
+                    child: Text(
+                      "Bienvenido a la",
+                      style: TextStyle(
+                          fontSize: texto1,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      "Familia Sol",
+                      style: TextStyle(
+                          fontSize: texto1,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  Center(
+                    child: Text(
+                      "Descubre las últimas novedades\n de la familia Sol",
+                      style: TextStyle(
+                          fontSize: texto2,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              )),
+            ),
+            Container(
+              //color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: ancho / botones,
+                    height: 45,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 255, 255, 255),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8))),
+                        child: Text(
+                          "Iniciar Sesión",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            
+                              fontSize: textoboton,
+                              fontWeight: FontWeight.w700,
+                              color: const Color.fromARGB(255, 61, 84, 207)),
+                        )),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  Container(
+                    width: ancho / botones,
+                    height: 45,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor:
+                                const Color.fromARGB(255, 61, 84, 207),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8))),
+                        child: Text(
+                          "Registrarse",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: textoboton,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        )),
+                  ),
+                ],
               ),
             ),
-          ),
-        ),
-        Center(
-          child: Text("Bienvenido a la",
-          style: TextStyle(
-            fontSize: texto,
-            color: Colors.white,fontWeight: FontWeight.w700),),
-        ),
-        Center(
-          child: Text("Familia Sol",
-          style: TextStyle(
-            fontSize: texto,
-            color: Colors.white,fontWeight: FontWeight.w700),),
-        ),
-      ],
-    );
+            const SizedBox(
+              height: 80,
+            ),
+          ],
+        ));
   }
 
   @override
@@ -48,10 +144,11 @@ class _PreloginState extends State<Prelogin> {
       body: Stack(
         children: [
           Container(
-            decoration:const  BoxDecoration(
+            decoration: const BoxDecoration(
               image: DecorationImage(
                 image: AssetImage('lib/imagenes/azul.jpg'),
-                fit: BoxFit.cover, // Cambiado a BoxFit.cover para que cubra todo el Container
+                fit: BoxFit
+                    .cover, // Cambiado a BoxFit.cover para que cubra todo el Container
               ),
             ),
           ),
@@ -60,16 +157,40 @@ class _PreloginState extends State<Prelogin> {
               LayoutBuilder(
                 builder: (context, constraints) {
                   if (constraints.maxWidth <= Breakpoint.xsmall) {
-                    return sollogo(200,200,14);
-                    
+                    //codigo,ancho,alto,t1,t2,botones,textoboton
+                    return sollogo("xsmall",100, constraints.maxWidth, 180, 23,
+                        15, 0.85, 12);
+                  } else if (constraints.maxWidth <= Breakpoint.avgsmall) {
+                    return sollogo("avgsmall", 130, constraints.maxWidth, 190,
+                        23, 15, 0.88, 16);
                   } else if (constraints.maxWidth <= Breakpoint.small) {
-                    return sollogo(250,300,20);
+                    return sollogo("small", 150, constraints.maxWidth, 220, 30,
+                        17, 0.8, 20);
+                  } else if (constraints.maxWidth <= Breakpoint.avgmedium) {
+                    return sollogo("avgmedium", 170, constraints.maxWidth, 230,
+                        23, 15, 0.77, 25);
                   } else if (constraints.maxWidth <= Breakpoint.medium) {
-                     return sollogo(320,350,25);
+                    return sollogo("medium", 200, constraints.maxWidth, 250, 29,
+                        20, 0.78, 25);
+                  } else if (constraints.maxWidth <= Breakpoint.avglarg) {
+                    return sollogo("avglarge", 220, constraints.maxWidth, 250,
+                        37, 20, 0.65, 25);
                   } else if (constraints.maxWidth <= Breakpoint.large) {
-                     return sollogo(380,490,35);
+                    return sollogo("large", 230, constraints.maxWidth, 250, 35,
+                        25, 0.62, 25);
+                  } else if (constraints.maxWidth <= Breakpoint.avgxlarge) {
+                    return sollogo("avgxlarge", 250, constraints.maxWidth, 250,
+                        43, 25, 0.55, 25);
                   } else {
-                    return sollogo(500,520,45);/*Text(
+                    return sollogo(
+                        "xlarge",
+                        270,
+                        constraints.maxWidth,
+                        280,
+                        40,
+                        25,
+                        0.55,
+                        35); /*Text(
                       "out range",
                       style: TextStyle(fontSize: 60),
                     );*/
@@ -82,7 +203,4 @@ class _PreloginState extends State<Prelogin> {
       ),
     );
   }
-
-
- 
 }
--- lib/componentes/responsiveUI/breakpoint.dart
+++ lib/componentes/responsiveUI/breakpoint.dart
@@ -1,7 +1,11 @@
 class Breakpoint {
-  static double xsmall = 320.0;
-  static double small = 576.0;
-  static double medium = 768.0;
-  static double large = 992.0;
-  static double xlarge = 1200.0;
+  static double xsmall = 320.00;      // Very small devices
+  static double avgsmall = 400.00;    // Small devices
+  static double small = 480.00;       // Small to medium devices
+  static double avgmedium = 600.00;   // Medium devices
+  static double medium = 720.00;      // Medium to large devices
+  static double avglarg = 840.00;     // Large devices / Small tablets
+  static double large = 960.00;       // Tablets
+  static double avgxlarge = 1080.00;  // Large tablets
+  static double xlarge = 1200.00;     // Extra large tablets and screens
 }
